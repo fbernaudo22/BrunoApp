c6923b3fba17a452612c78af912d431488f78db8
fbernaudo22 <113998875+fbernaudo22@users.noreply.github.com>
2022-12-14 14:02:41 +0100

    Update ListView.swift

--- PetSitter/Views/ListView.swift
+++ PetSitter/Views/ListView.swift
@@ -16,46 +16,64 @@ struct ListView: View {
     var filteredItems: [petSitter] {
         if search.isEmpty {
             return modelData.petsitters}
-            else {
-                return modelData.petsitters.filter {$0.name.localizedCaseInsensitiveContains(search) || $0.rating.localizedCaseInsensitiveContains(search) || $0.animals.localizedCaseInsensitiveContains(search)}
-            }
+        else {
+            return modelData.petsitters.filter {$0.name.localizedCaseInsensitiveContains(search) || $0.rating.localizedCaseInsensitiveContains(search) || $0.animals.localizedCaseInsensitiveContains(search)}
+        }
     }
     var body: some View {
         NavigationStack{
-            VStack {
-                HStack{
-                    Text("Ordered by:")
-                    Spacer()
-                    Picker("Ordered by:", selection: $selectedItem) {
-                        ForEach(pickerOptions, id: \.self) { option in
-                            Text(option)
+            ZStack{
+                VStack {
+                    HStack{
+                        Text("Ordered by:")
+                        Spacer()
+                        Picker("Ordered by:", selection: $selectedItem) {
+                            ForEach(pickerOptions, id: \.self) { option in
+                                Text(option)
+                            }
                         }
+                    }.padding(.horizontal)
+                    ScrollView(showsIndicators: false) {
+                        ForEach(filteredItems) { sitter in
+                            NavigationLink(destination: PetSitterDetail(sitter: sitter, region: MKCoordinateRegion(center: sitter.locationCoordinates, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))), label:{PetSitterCard(sitter: sitter)
+                            })
+                        }.searchable(text: $search)
                     }
                 }
-                ScrollView {
-                    ForEach(filteredItems) { sitter in
-                        PetSitterCard(sitter: sitter)
-                    }//.searchable(text: $search)
-                }.searchable(text: $search)
-            }
-            .padding()
-            .navigationTitle("Dashboard")
-            .navigationBarTitleDisplayMode(.automatic)
-            .toolbar {
-                ToolbarItem(placement: .automatic,   content: {NavigationLink( destination: ProfileView(),
-                                                                               label: {
-                    Image("preview_profile")
-                        .resizable()
-                        .scaledToFit()
+                VStack{
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        NavigationLink(destination: MapView(), label: {
+                            Image(systemName: "map.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        })
+                        .buttonStyle(.borderedProminent)
                         .clipShape(Circle())
-                        .shadow(radius: 2)
-                        .frame(width: 50, height: 50)
-                })}
-                            
-                )}
+                        .padding(.horizontal)
+                    }
+                }
+                }
+                .navigationTitle("Dashboard")
+                .navigationBarTitleDisplayMode(.automatic)
+                .toolbar {
+                    ToolbarItem(placement: .automatic,   content: {NavigationLink( destination: ProfileView(),
+                                                                                   label: {
+                        Image("preview_profile")
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .shadow(radius: 2)
+                            .frame(width: 50, height: 50)
+                    })}
+                                
+                    )}
+                .navigationBarBackButtonHidden()
+            }
         }
     }
-}
 
 struct ListView_Previews: PreviewProvider {
     static let modelData = ModelData()
--- PetSitter/Views/MapView.swift
+++ PetSitter/Views/MapView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    //@Binding var buttonIsActive: Bool
     @EnvironmentObject var modelData: ModelData
     @StateObject private var locationManager = LocationManager()
     var region: Binding<MKCoordinateRegion>? {
@@ -26,9 +27,24 @@ struct MapView: View {
                 Map(coordinateRegion: region!,interactionModes: .all, showsUserLocation: true, userTrackingMode: .constant(.none), annotationItems: modelData.petsitters) {location in
                     MapAnnotation(coordinate: location.locationCoordinates) { NavigationLink(destination: PetSitterDetail(sitter: location, region: MKCoordinateRegion(center: location.locationCoordinates, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))), label: {placeholder(imageName: location.imageName)
                     })
-                    }}
-            }.ignoresSafeArea(edges: .top)
-                .padding(.bottom)
+                    }}.ignoresSafeArea()
+                VStack{
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        NavigationLink(destination: ListView(), label: {
+                            Image(systemName: "house.circle")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        })
+                        .buttonStyle(.borderedProminent)
+                        .clipShape(Circle())
+                        .padding(.horizontal)
+                        .navigationBarBackButtonHidden()
+                    }
+                }
+            }
         }
         
     }
