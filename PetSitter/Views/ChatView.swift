46c2a001dd866389d40ba294ff744d262cc827e8
Hendrix88 <amura22@fed.idserve.net>
2022-12-15 14:16:21 +0100

    Boh

--- PetSitter/Views/ChatView.swift
+++ PetSitter/Views/ChatView.swift
@@ -26,7 +26,7 @@ struct ChatView: View {
             }))
         }
         .sheet(isPresented: $isPresented) {
-            HelloWorldView()
+            //HelloWorldView()
         }
     }
     
