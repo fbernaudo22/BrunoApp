0f4d877d4a0c0051e9b5f17796ef848687b8de5f
fbernaudo22 <113998875+fbernaudo22@users.noreply.github.com>
2022-12-15 14:38:31 +0100

    Update dogsitterData.json

--- PetSitter/PetSitterApp.swift
+++ PetSitter/PetSitterApp.swift
@@ -14,6 +14,7 @@ struct PetSitterApp: App {
         WindowGroup {
             ContentView()
                 .environmentObject(ModelData())
+                .accentColor(.arancione)
         }
     }
 }
